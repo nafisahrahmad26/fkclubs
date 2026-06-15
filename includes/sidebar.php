a043c39c97a40b2d4feb2a10f324289aad4bb023
sis4naa <156654635+sis4naa@users.noreply.github.com>
2026-06-15 15:09:12 +0800

    Add files via upload

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -7,7 +7,9 @@
                     <li><a href="../module1/user_management.php">Users</a></li>
                 <?php endif; ?>
                 <li><a href="../module2/club_list.php">Clubs</a></li>
+                <?php if($_SESSION['user_type'] !== 'Admin'): ?>
                 <li><a href="../module3/event_management.php">Events</a></li>
+                <?php endif; ?>
                 <li><a href="../module4/attendance.php">Attendance</a></li>
                 <li><a href="../module4/participant_reports.php">Reports</a></li>
                 <li><a href="../module1/profile.php">Profile</a></li>
